0fa77c1299d22c3a28dab3fc24fb950a358e4bd3
grammka <grammka@gmail.com>
2016-07-27 16:32:04 +0300

    added 22 game

---
+++ data/storage/transformed_games/game_22.js
@@ -0,0 +1,211 @@
+module.exports = {
+  "minPoints": 17.5,
+  "maxPoints": 56.5,
+  "difficultyRate": 1.133,
+  "teams": {
+    "Братья по умолчанию": {
+      "name": "Братья по умолчанию",
+      "totalPoints": 56.5,
+      "winPercent": 100,
+      "rounds": []
+    },
+    "УО": {
+      "name": "УО",
+      "totalPoints": 54.5,
+      "winPercent": 96.46,
+      "rounds": []
+    },
+    "Ласковый отчим": {
+      "name": "Ласковый отчим",
+      "totalPoints": 45,
+      "winPercent": 79.65,
+      "rounds": []
+    },
+    "ВАТА и партнеры": {
+      "name": "ВАТА и партнеры",
+      "totalPoints": 44,
+      "winPercent": 77.88,
+      "rounds": []
+    },
+    "Тайная комната 218": {
+      "name": "Тайная комната 218",
+      "totalPoints": 42,
+      "winPercent": 74.34,
+      "rounds": []
+    },
+    "Слабоумие и отвага": {
+      "name": "Слабоумие и отвага",
+      "totalPoints": 42,
+      "winPercent": 74.34,
+      "rounds": []
+    },
+    "У команды нет имени": {
+      "name": "У команды нет имени",
+      "totalPoints": 41,
+      "winPercent": 72.57,
+      "rounds": []
+    },
+    "Капитальные красавчики": {
+      "name": "Капитальные красавчики",
+      "totalPoints": 41,
+      "winPercent": 72.57,
+      "rounds": []
+    },
+    "МосБлог": {
+      "name": "МосБлог",
+      "totalPoints": 39.5,
+      "winPercent": 69.91,
+      "rounds": []
+    },
+    "Пираты Эквадора": {
+      "name": "Пираты Эквадора",
+      "totalPoints": 39,
+      "winPercent": 69.03,
+      "rounds": []
+    },
+    "Магнезия в 12": {
+      "name": "Магнезия в 12",
+      "totalPoints": 39,
+      "winPercent": 69.03,
+      "rounds": []
+    },
+    "Енот, Why Not?": {
+      "name": "Енот, Why Not?",
+      "totalPoints": 38,
+      "winPercent": 67.26,
+      "rounds": []
+    },
+    "Позитивчик": {
+      "name": "Позитивчик",
+      "totalPoints": 37.5,
+      "winPercent": 66.37,
+      "rounds": []
+    },
+    "Ядрёны пчёлы": {
+      "name": "Ядрёны пчёлы",
+      "totalPoints": 37,
+      "winPercent": 65.49,
+      "rounds": []
+    },
+    "Магнезия в 11": {
+      "name": "Магнезия в 11",
+      "totalPoints": 36.5,
+      "winPercent": 64.6,
+      "rounds": []
+    },
+    "Португальцы": {
+      "name": "Португальцы",
+      "totalPoints": 36.5,
+      "winPercent": 64.6,
+      "rounds": []
+    },
+    "о.крошка!": {
+      "name": "о.крошка!",
+      "totalPoints": 35.5,
+      "winPercent": 62.83,
+      "rounds": []
+    },
+    "7.13": {
+      "name": "7.13",
+      "totalPoints": 35.5,
+      "winPercent": 62.83,
+      "rounds": []
+    },
+    "Клуб пессимистов": {
+      "name": "Клуб пессимистов",
+      "totalPoints": 35.5,
+      "winPercent": 62.83,
+      "rounds": []
+    },
+    "Эстеты-экстремисты": {
+      "name": "Эстеты-экстремисты",
+      "totalPoints": 35,
+      "winPercent": 61.95,
+      "rounds": []
+    },
+    "Дружба": {
+      "name": "Дружба",
+      "totalPoints": 34,
+      "winPercent": 60.18,
+      "rounds": []
+    },
+    "Парижская синь": {
+      "name": "Парижская синь",
+      "totalPoints": 34,
+      "winPercent": 60.18,
+      "rounds": []
+    },
+    "МиниСтерВство": {
+      "name": "МиниСтерВство",
+      "totalPoints": 34,
+      "winPercent": 60.18,
+      "rounds": []
+    },
+    "Анаконда": {
+      "name": "Анаконда",
+      "totalPoints": 33,
+      "winPercent": 58.41,
+      "rounds": []
+    },
+    "6 из 45": {
+      "name": "6 из 45",
+      "totalPoints": 32.5,
+      "winPercent": 57.52,
+      "rounds": []
+    },
+    "Безумные люди": {
+      "name": "Безумные люди",
+      "totalPoints": 31.5,
+      "winPercent": 55.75,
+      "rounds": []
+    },
+    "Brain Drain": {
+      "name": "Brain Drain",
+      "totalPoints": 31,
+      "winPercent": 54.87,
+      "rounds": []
+    },
+    "EtCetera": {
+      "name": "EtCetera",
+      "totalPoints": 30.5,
+      "winPercent": 53.98,
+      "rounds": []
+    },
+    "Друзья Друзя": {
+      "name": "Друзья Друзя",
+      "totalPoints": 28.5,
+      "winPercent": 50.44,
+      "rounds": []
+    },
+    "Жеванные кроты": {
+      "name": "Жеванные кроты",
+      "totalPoints": 26,
+      "winPercent": 46.02,
+      "rounds": []
+    },
+    "Баранинка": {
+      "name": "Баранинка",
+      "totalPoints": 25,
+      "winPercent": 44.25,
+      "rounds": []
+    },
+    "Додо": {
+      "name": "Додо",
+      "totalPoints": 23,
+      "winPercent": 40.71,
+      "rounds": []
+    },
+    "Сиси рыси": {
+      "name": "Сиси рыси",
+      "totalPoints": 22.5,
+      "winPercent": 39.82,
+      "rounds": []
+    },
+    "Баррель": {
+      "name": "Баррель",
+      "totalPoints": 17.5,
+      "winPercent": 30.97,
+      "rounds": []
+    }
+  }
+};
