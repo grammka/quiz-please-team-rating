1462f3f482bf887db1d6ed9fa946e5f8296ac2e9
grammka <grammka@gmail.com>
2016-07-29 13:38:20 +0300

    added 23 game

---
+++ data/storage/transformed_games/game_23.js
@@ -0,0 +1,115 @@
+module.exports = {
+  "minPoints": 9.5,
+  "maxPoints": 40.5,
+  "difficultyRate": 1.58,
+  "teams": {
+    "Парижская синь": {
+      "name": "Парижская синь",
+      "totalPoints": 40.5,
+      "winPercent": 100,
+      "rounds": []
+    },
+    "31-я секунда": {
+      "name": "31-я секунда",
+      "totalPoints": 33.5,
+      "winPercent": 82.72,
+      "rounds": []
+    },
+    "Отрыжка": {
+      "name": "Отрыжка",
+      "totalPoints": 28.5,
+      "winPercent": 70.37,
+      "rounds": []
+    },
+    "С детства за Лестер": {
+      "name": "С детства за Лестер",
+      "totalPoints": 27.5,
+      "winPercent": 67.9,
+      "rounds": []
+    },
+    "Ласковый отчим": {
+      "name": "Ласковый отчим",
+      "totalPoints": 27,
+      "winPercent": 66.67,
+      "rounds": []
+    },
+    "Енот, Why Not?": {
+      "name": "Енот, Why Not?",
+      "totalPoints": 27,
+      "winPercent": 66.67,
+      "rounds": []
+    },
+    "Трудно быть богом": {
+      "name": "Трудно быть богом",
+      "totalPoints": 26,
+      "winPercent": 64.2,
+      "rounds": []
+    },
+    "Могила Иммануила": {
+      "name": "Могила Иммануила",
+      "totalPoints": 25,
+      "winPercent": 61.73,
+      "rounds": []
+    },
+    "Олимпийское спокойствие": {
+      "name": "Олимпийское спокойствие",
+      "totalPoints": 25,
+      "winPercent": 61.73,
+      "rounds": []
+    },
+    "Кукуцаполь": {
+      "name": "Кукуцаполь",
+      "totalPoints": 23,
+      "winPercent": 56.79,
+      "rounds": []
+    },
+    "Братья по умолчанию": {
+      "name": "Братья по умолчанию",
+      "totalPoints": 22.5,
+      "winPercent": 55.56,
+      "rounds": []
+    },
+    "ВАТА и партнёры": {
+      "name": "ВАТА и партнёры",
+      "totalPoints": 22.5,
+      "winPercent": 55.56,
+      "rounds": []
+    },
+    "Слабоумие и отвага": {
+      "name": "Слабоумие и отвага",
+      "totalPoints": 21,
+      "winPercent": 51.85,
+      "rounds": []
+    },
+    "Сиси-Рыси": {
+      "name": "Сиси-Рыси",
+      "totalPoints": 20,
+      "winPercent": 49.38,
+      "rounds": []
+    },
+    "2+2": {
+      "name": "2+2",
+      "totalPoints": 19.5,
+      "winPercent": 48.15,
+      "rounds": []
+    },
+    "Комфортные корги": {
+      "name": "Комфортные корги",
+      "totalPoints": 18.5,
+      "winPercent": 45.68,
+      "rounds": []
+    },
+    "В Питере пить": {
+      "name": "В Питере пить",
+      "totalPoints": 15.5,
+      "winPercent": 38.27,
+      "rounds": []
+    },
+    "Безумные люди": {
+      "name": "Безумные люди",
+      "totalPoints": 9.5,
+      "winPercent": 23.46,
+      "rounds": []
+    }
+  }
+};
