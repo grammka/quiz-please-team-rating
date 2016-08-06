80c76f6124c70e5848ae7fa98149bb33e6e3ed6a
grammka <grammka@gmail.com>
2016-08-06 18:09:34 +0300

    added 24 game

---
+++ data/storage/transformed_games/game_24.js
@@ -0,0 +1,229 @@
+module.exports = {
+  "minPoints": 21.5,
+  "maxPoints": 55,
+  "difficultyRate": 1.164,
+  "teams": {
+    "Слабоумие и отвага": {
+      "name": "Слабоумие и отвага",
+      "totalPoints": 55,
+      "winPercent": 100,
+      "rounds": []
+    },
+    "7.13": {
+      "name": "7.13",
+      "totalPoints": 52.5,
+      "winPercent": 95.45,
+      "rounds": []
+    },
+    "Капитальные красавчики": {
+      "name": "Капитальные красавчики",
+      "totalPoints": 51.5,
+      "winPercent": 93.64,
+      "rounds": []
+    },
+    "Ежу понятно": {
+      "name": "Ежу понятно",
+      "totalPoints": 51.5,
+      "winPercent": 93.64,
+      "rounds": []
+    },
+    "Эстеты-экстремисты": {
+      "name": "Эстеты-экстремисты",
+      "totalPoints": 51,
+      "winPercent": 92.73,
+      "rounds": []
+    },
+    "Департамент Деградации": {
+      "name": "Департамент Деградации",
+      "totalPoints": 50,
+      "winPercent": 90.91,
+      "rounds": []
+    },
+    "Олимпийское спокойствие": {
+      "name": "Олимпийское спокойствие",
+      "totalPoints": 49.5,
+      "winPercent": 90,
+      "rounds": []
+    },
+    "Парижская синь": {
+      "name": "Парижская синь",
+      "totalPoints": 48.5,
+      "winPercent": 88.18,
+      "rounds": []
+    },
+    "Енот, Why Not?": {
+      "name": "Енот, Why Not?",
+      "totalPoints": 48.5,
+      "winPercent": 88.18,
+      "rounds": []
+    },
+    "УО": {
+      "name": "УО",
+      "totalPoints": 48,
+      "winPercent": 87.27,
+      "rounds": []
+    },
+    "Комфортные корги": {
+      "name": "Комфортные корги",
+      "totalPoints": 48,
+      "winPercent": 87.27,
+      "rounds": []
+    },
+    "Магнезия в 11": {
+      "name": "Магнезия в 11",
+      "totalPoints": 47.5,
+      "winPercent": 86.36,
+      "rounds": []
+    },
+    "Гре моя тачка, чуваш?": {
+      "name": "Гре моя тачка, чуваш?",
+      "totalPoints": 47.5,
+      "winPercent": 86.36,
+      "rounds": []
+    },
+    "Отрыжка": {
+      "name": "Отрыжка",
+      "totalPoints": 47,
+      "winPercent": 85.45,
+      "rounds": []
+    },
+    "Мизантропы": {
+      "name": "Мизантропы",
+      "totalPoints": 47,
+      "winPercent": 85.45,
+      "rounds": []
+    },
+    "Дружба": {
+      "name": "Дружба",
+      "totalPoints": 46,
+      "winPercent": 83.64,
+      "rounds": []
+    },
+    "Сиси-Рыси": {
+      "name": "Сиси-Рыси",
+      "totalPoints": 46,
+      "winPercent": 83.64,
+      "rounds": []
+    },
+    "ВАТА и партнеры": {
+      "name": "ВАТА и партнеры",
+      "totalPoints": 45.5,
+      "winPercent": 82.73,
+      "rounds": []
+    },
+    "МОРОС": {
+      "name": "МОРОС",
+      "totalPoints": 45.5,
+      "winPercent": 82.73,
+      "rounds": []
+    },
+    "Гнилые кавалеры": {
+      "name": "Гнилые кавалеры",
+      "totalPoints": 45.5,
+      "winPercent": 82.73,
+      "rounds": []
+    },
+    "Мы пришли поесть": {
+      "name": "Мы пришли поесть",
+      "totalPoints": 44,
+      "winPercent": 80,
+      "rounds": []
+    },
+    "Братья по умолчанию": {
+      "name": "Братья по умолчанию",
+      "totalPoints": 43,
+      "winPercent": 78.18,
+      "rounds": []
+    },
+    "16 булок": {
+      "name": "16 булок",
+      "totalPoints": 42,
+      "winPercent": 76.36,
+      "rounds": []
+    },
+    "Анаконда": {
+      "name": "Анаконда",
+      "totalPoints": 40.5,
+      "winPercent": 73.64,
+      "rounds": []
+    },
+    "Огурцы": {
+      "name": "Огурцы",
+      "totalPoints": 40,
+      "winPercent": 72.73,
+      "rounds": []
+    },
+    "Позитивчик": {
+      "name": "Позитивчик",
+      "totalPoints": 39,
+      "winPercent": 70.91,
+      "rounds": []
+    },
+    "Brain Drain": {
+      "name": "Brain Drain",
+      "totalPoints": 39,
+      "winPercent": 70.91,
+      "rounds": []
+    },
+    "МосБлог": {
+      "name": "МосБлог",
+      "totalPoints": 39,
+      "winPercent": 70.91,
+      "rounds": []
+    },
+    "Атлас": {
+      "name": "Атлас",
+      "totalPoints": 38,
+      "winPercent": 69.09,
+      "rounds": []
+    },
+    "Пьер Решал": {
+      "name": "Пьер Решал",
+      "totalPoints": 37.5,
+      "winPercent": 68.18,
+      "rounds": []
+    },
+    "Зеленый забор": {
+      "name": "Зеленый забор",
+      "totalPoints": 37,
+      "winPercent": 67.27,
+      "rounds": []
+    },
+    "МинистрВство": {
+      "name": "МинистрВство",
+      "totalPoints": 35,
+      "winPercent": 63.64,
+      "rounds": []
+    },
+    "Баранинка": {
+      "name": "Баранинка",
+      "totalPoints": 34,
+      "winPercent": 61.82,
+      "rounds": []
+    },
+    "Трудно быть богом": {
+      "name": "Трудно быть богом",
+      "totalPoints": 33.5,
+      "winPercent": 60.91,
+      "rounds": []
+    },
+    "Клуб пессимистов": {
+      "name": "Клуб пессимистов",
+      "totalPoints": 30,
+      "winPercent": 54.55,
+      "rounds": []
+    },
+    "Солнцеподобные": {
+      "name": "Солнцеподобные",
+      "totalPoints": 26,
+      "winPercent": 47.27,
+      "rounds": []
+    },
+    "Сборная городов": {
+      "name": "Сборная городов",
+      "totalPoints": 21.5,
+      "winPercent": 39.09,
+      "rounds": []
+    }
+  }
+};
